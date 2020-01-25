77611a7e6208e0f1854e443ef866d3d81be905d7
Kristin Lam <klam623@gmail.com>
2020-01-25 14:58:23 -0500

    Create Paper.js

---
+++ src/Paper.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Paper () {
+    return (
+      <div>
+        <h2>Paper</h2>
+        <ul>
+          <li>Cardboard</li>
+          <li>Computer paper</li>
+        </ul>
+      </div>
+    );
+  }
+
+export default Paper;
