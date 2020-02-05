a2a67100735198e64459ec89ff8556f592cdcab8
Kristin Lam <lamk1@mskcc.org>
2020-02-05 16:09:37 -0500

    add filler text for Contact

--- src/Contact.js
+++ src/Contact.js
@@ -6,7 +6,11 @@ function Contact () {
       <div className="section">
         <div className="textbox">
           <h2 className="heading">III.</h2>
-          <p>bruuuuuuuuuuuuuuh</p>
+          <p>Reach out to me at <a href="mailto:mrayd92@gmail.com">mrayd92@gmail.com</a>.</p>
+          <ul>
+            <li><a href="https://www.linkedin.com/in/michael-dufauchard-1406836a/" target="_blank">LinkedIn</a></li>
+            <li><a href="https://github.com/michaeldufauchard" target="_blank">Github</a></li>
+          </ul>
         </div>
       </div>
     );
