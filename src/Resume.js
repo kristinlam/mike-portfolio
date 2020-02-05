b2b91a3850087ac096f2abcb6c37b8df425143f4
Kristin Lam <lamk1@mskcc.org>
2020-02-05 16:10:39 -0500

    add skillbars

--- src/Resume.js
+++ src/Resume.js
@@ -1,12 +1,29 @@
 import React from "react";
+import SkillBar from 'react-skills-bars';
 import './section.css';
+import './skillbars.css';
+import SkillBars from './SkillBars.js'
+
+const skills = [
+  {type: "VBA", level: 100},
+  {type: "SQL", level: 95},
+  {type: "Python", level: 95},
+  {type: "C", level: 90},
+  {type: "C++", level: 90},
+  {type: "Java", level: 90},
+  {type: "Matlab", level: 80},
+  {type: "JavaScript", level: 85},
+  {type: "HTML", level: 90},
+  {type: "CSS", level: 90}
+];
 
 function Resume () {
     return (
-      <div>
+      <div className="section">
         <div className="textbox">
-          <h2 className="heading">Resume</h2>
-          <p>Information here.</p>
+          <h2 className="heading">II.</h2>
+          <h3 className="subheading">Skills</h3>
+          <SkillBars hue="200" saturation="10" skills={skills} />
         </div>
       </div>
     );
