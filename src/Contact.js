7c1083fc19f6e4e8bfefcddacab460f0e05c3483
Kristin Lam <klam623@gmail.com>
2020-02-05 23:10:30 -0500

    Update Contact.js

--- src/Contact.js
+++ src/Contact.js
@@ -6,11 +6,22 @@ function Contact () {
       <div className="section">
         <div className="textbox">
           <h2 className="heading">III.</h2>
-          <p>Reach out to me at <a href="mailto:mrayd92@gmail.com">mrayd92@gmail.com</a>.</p>
-          <ul>
-            <li><a href="https://www.linkedin.com/in/michael-dufauchard-1406836a/" target="_blank">LinkedIn</a></li>
-            <li><a href="https://github.com/michaeldufauchard" target="_blank">Github</a></li>
-          </ul>
+
+          <div className="connect">
+            <h3 className="subheading">For any inquiries</h3>
+            <a href="mailto:mrayd92@gmail.com">mrayd92@gmail.com</a>
+          </div>
+          
+          <div className="connect">
+            <h3 className="subheading">To connect online</h3>
+            <a href="https://www.linkedin.com/in/michael-dufauchard/" target="_blank">linkedin.com/michael-dufauchard</a>
+          </div>
+
+          <div className="connect">
+            <h3 className="subheading">For examples of my code</h3>
+            <a href="https://github.com/michaeldufauchard" target="_blank">github.com/michaeldufauchard</a>
+          </div>
+
         </div>
       </div>
     );
